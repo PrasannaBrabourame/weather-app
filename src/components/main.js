9e9ac5f0f4c0ad94d39dd9cffff5c9a4c825dcbc
Prasanna Brabourame <IS5442@integra.co.in>
2018-11-28 11:57:48 +0530

    HandleClick

--- src/components/main.js
+++ src/components/main.js
@@ -8,37 +8,42 @@ export default class Main extends Component {
             _state:'Chennai',
             _country:'IN'
         }
+        this.handleButton = this.handleButton.bind(this);
     }
-  render() {
-    return (
-      <div className="main-section">
-      <div className="loan">
-      <h2 className='text-center'>Weather Application</h2>
-            <div className='text-center'>Prasanna Brabourame</div>
-            <div className="clear"></div>
-            <div className="line"></div>
-            <div className="form-left app-col-12 form_operation" >
-              <div className="form-row">
-                <div className="app-col-6 form-left">
-                    <label>State</label>
-                    <input type="text" required name="State" onChange={(e)=>this.handleChangeText(e)} value={this.state._state} disabled/>
-                </div>
-                <div className="app-col-6">
-                <label>Country</label>
-                    <input type="text" required name="amount" onChange={(e)=>this.handleChangeText(e)} value={this.state._country} disabled/>
-                </div>
-              </div>
+
+    handleButton(){
+
+    }
+    render() {
+      return (
+        <div className="main-section">
+        <div className="loan">
+        <h2 className='text-center'>Weather Application</h2>
+              <div className='text-center'>Prasanna Brabourame</div>
               <div className="clear"></div>
-            </div>
-            <div className="form-row text-center">
-                <button className="submit-btn" disabled >
-                    Get Weather
-              </button>
-            </div>
-            <div className="line"></div>
-            <SimpleSlider/>
-      </div>
-      </div>
+              <div className="line"></div>
+              <div className="form-left app-col-12 form_operation" >
+                <div className="form-row">
+                  <div className="app-col-6 form-left">
+                      <label>State</label>
+                      <input type="text" required name="State" onChange={(e)=>this.handleChangeText(e)} value={this.state._state} disabled/>
+                  </div>
+                  <div className="app-col-6">
+                  <label>Country</label>
+                      <input type="text" required name="amount" onChange={(e)=>this.handleChangeText(e)} value={this.state._country} disabled/>
+                  </div>
+                </div>
+                <div className="clear"></div>
+              </div>
+              <div className="form-row text-center">
+                  <button className="submit-btn" onClick={(e)=>this.handleButton(e)} disabled>
+                      Get Weather
+                  </button>
+              </div>
+              <div className="line"></div>
+              <SimpleSlider/>
+        </div>
+        </div>
     )
   }
 }
