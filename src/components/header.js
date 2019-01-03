c36826d1b0d8e92145ceaa5c1f4c909847d1114b
Prasanna Brabourame <IS5442@integra.co.in>
2019-01-03 09:32:59 +0530

    build commit

--- src/components/header.js
+++ src/components/header.js
@@ -5,12 +5,12 @@ export default class Header extends Component {
     return (
         <div className="header">
         <a href="#test">
-            <img src="http://www.auxolabs.in/img/logo-small.png" alt="Logo" className="logo"/>
+            <img src="https://logos.textgiraffe.com/logos/logo-name/Prasanna-designstyle-kiddo-m.png" alt="Logo" className="logo"/>
         </a>
         <div className="headerRight">
             <div></div>
             <button type="button" className="btn">
-                    <span>Auxo-Labs</span>
+                    <span>Prasanna Brabourame</span>
             </button>
         </div>
         
